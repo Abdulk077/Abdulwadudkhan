f671de1934c5fd3aa7bd33cefc33cacdb7e43cb3
Abdulk077 <Khanabdulwadud3@gmail.com>
2025-01-15 22:25:36 +0530

    First version of my portfoli

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-{/*import logo from '../assets/logo.png';*/}
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
@@ -8,7 +7,6 @@ const Navbarcomp = () => {
     return (
       <nav className="mb-20 flex iteams-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-          {/*<img className="mx-20 w-12 text-white bg bg-white" src={logo} alt="logo"/>*/}
           <h1 className="text-4xl mx-20 font-extralight">AB</h1>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
